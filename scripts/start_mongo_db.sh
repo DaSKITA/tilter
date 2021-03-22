c5c2ff0d3c2507da9dd51a701f1c9e736127790c
Michael Gebauer | TU <mg@ise.tu-berlin.de>
2021-03-22 14:29:55 -0700

    add mongo db startup script

--- scripts/start_mongo_db.sh
+++ scripts/start_mongo_db.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 BASEDIR=$(dirname "$SCRIPT_DIR")
 ENV_FILE_DIR="${BASEDIR}/app/secrets/local/mongodb-local.env"
 
-docker run -p 27017:27017 --env-file=$ENV_FILE_DIR --net tmp mongo:4.4.4
+docker run -p 27017:27017 --env-file=$ENV_FILE_DIR mongo:4.4.4
