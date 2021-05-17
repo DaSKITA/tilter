85087a18f863532ef1089382b99edda24c7f40ad
Michael Gebauer | TU <mg@ise.tu-berlin.de>
2021-05-17 02:51:11 -0700

    added documentation for annotation handler

--- app/tilt_resources/annotation_handler.py
+++ app/tilt_resources/annotation_handler.py
@@ -10,6 +10,18 @@ class AnnotationHandler:
         self.new_annotations = []
 
     def get_annotation(self, task=None, text=None, start=None, end=None, label=None) -> Union[Annotation, None]:
+        """Retrieves an Annotation by values. If the Annotation does not exist an error is thrown.
+
+        Args:
+            task ([type], optional): [description]. Defaults to None.
+            text ([type], optional): [description]. Defaults to None.
+            start ([type], optional): [description]. Defaults to None.
+            end ([type], optional): [description]. Defaults to None.
+            label ([type], optional): [description]. Defaults to None.
+
+        Returns:
+            Union[Annotation, None]: [description]
+        """
         try:
             annotation = Annotation.objects.get(task=task, text=text, start=start, end=end, label=label)
         except DoesNotExist:
@@ -19,6 +31,20 @@ class AnnotationHandler:
 
     def create_and_save_annotation(self, task=None, text=None, start=None, end=None, label=None,
                                    return_annotation=False) -> Tuple[bool, Union[Annotation, None]]:
+        """Creates an nnotation. Before the annotation is created it is checked, whether it already exists in
+        the database. If it existed already the boolean value in the returned tuple will be set to false.
+
+        Args:
+            task ([type], optional): [description]. Defaults to None.
+            text ([type], optional): [description]. Defaults to None.
+            start ([type], optional): [description]. Defaults to None.
+            end ([type], optional): [description]. Defaults to None.
+            label ([type], optional): [description]. Defaults to None.
+            return_annotation (bool, optional): [description]. Defaults to False.
+
+        Returns:
+            Tuple[bool, Union[Annotation, None]]: [description]
+        """
         annotation = self.get_annotation(task=task, text=text, start=start, end=end, label=label)
         if not annotation:
             annotation = Annotation(task=task, text=text, start=start, end=end, label=label)
@@ -35,6 +61,17 @@ class AnnotationHandler:
             print(f"Deleted Annotation with Label: {annotation.label}")
 
     def filter_new_annotations(self, annotation_list):
+        """
+        Filters our new annotations from a list of given annotation values. Every new annotation will be
+        created. A list of newly created annotations, and all annotations generated from the provided list
+        will be created.
+
+        Args:
+            annotation_list ([type]): [description]
+
+        Returns:
+            [type]: [description]
+        """
         for annotation_arguments in annotation_list:
             created, annotation = self.create_and_save_annotation(**annotation_arguments,
                                                                   return_annotation=True)
@@ -44,6 +81,13 @@ class AnnotationHandler:
         return self.new_annotations, self.all_current_annotations
 
     def synch_task_annotations(self, task, current_annotation_list):
+        """
+        Deletes annotations in the list from the database that are not part of the provided task.
+
+        Args:
+            task ([type]): [description]
+            current_annotation_list ([type]): [description]
+        """
         for anno in Annotation.objects(task=task):
             if anno.id not in current_annotation_list:
                 self.delete(anno)
