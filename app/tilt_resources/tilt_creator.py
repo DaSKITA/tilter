f44fbd4e3b626c0b2220adf243f67f7d75193494
Michael Gebauer | TU <mg@ise.tu-berlin.de>
2021-05-31 00:34:39 -0700

    added minor documentation

--- app/tilt_resources/tilt_creator.py
+++ app/tilt_resources/tilt_creator.py
@@ -5,6 +5,14 @@ from typing import Dict, Union
 
 @dataclass
 class TiltException:
+    """
+    A class for storing tilt exceptions. Every exceptions contains a schema_key_queue and a
+    tilt_exception entry. The schema_key_queue is a path through the tilt_document. The tilt_exception_entry
+    is the default value, for the given path in the schema_key_queue.
+
+    Returns:
+        [type]: [description]
+    """
     schema_key_queue: list = None
     tilt_exception_entry: Union[int, str] = None
 
@@ -37,11 +45,31 @@ class TiltCreator:
             self.add_tilt_exception(tilt_exception_obj)
 
     def write_tilt_default_values(self) -> Dict:
+        """
+        Iterates over exception classes to retrieve default values. Every default value is written in the
+        config.py. Every iteration the tilt document stored in the class variables in updated.
+
+        Returns:
+            Dict: [description]
+        """
         for tilt_exception in self.tilt_exceptions:
             updated_tilt_document = self._place_default_values(tilt_exception, self.tilt_document)
             self.tilt_document = updated_tilt_document
 
-    def _place_default_values(self, tilt_exception: 'TiltException', tilt_document: Dict = None):
+    def _place_default_values(self, tilt_exception: 'TiltException', tilt_document: Dict = None) -> Dict:
+        """
+        Iterates through a given tilt_document recursively. Based in the type of the retrieved entry another
+        recursive call is triggered or the existinng value in the exception class is added under the
+        respective key. Iteration is basically done over a "schema_key_queue", which is a list full of
+        labels from the tilt schema. This list can be seen as a path through the tilt_document.
+
+        Args:
+            tilt_exception (TiltException): [description]
+            tilt_document (Dict, optional): [description]. Defaults to None.
+
+        Returns:
+            Dict: [description]
+        """
         schema_key = tilt_exception.schema_key_queue.pop(0)
         if isinstance(tilt_document, list):
             for idx, _ in enumerate(tilt_document):
