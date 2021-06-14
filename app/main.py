7cda26e52e91ce315b4300c1b4a3ee6eafd06973
fmaschhur <f.maschhur@live.de>
2021-06-14 12:30:39 +0200

    fixed error messages
    Signed-off-by: fmaschhur <f.maschhur@live.de>

--- app/main.py
+++ app/main.py
@@ -171,13 +171,13 @@ class Authentication(Resource):
         try:
             current_user = User.objects.get(username=username)
         except Exception as e:
-            return {"msg": "Username does not exist"}, 401
+            return {"msg": "Wrong username or password"}, 401
 
         if user_manager.password_manager.verify_password(password=password, password_hash=current_user.password):
             access_token = create_access_token(identity=username)
             return access_token, 200
         else:
-            return {"msg": "Wrong password"}, 401
+            return {"msg": "Wrong username or password"}, 401
 
 
 
