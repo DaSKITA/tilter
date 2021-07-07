75fada25a5a3950bcbef1e90b8984f16df0ba40c
Michael Gebauer | TU <mg@ise.tu-berlin.de>
2021-07-07 05:30:13 -0700

    moved migration

--- scripts/database_migration.sh
+++ scripts/database_migration.sh
@@ -3,6 +3,6 @@
 SCRIPT_DIR=$(cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )
 BASEDIR=$(dirname "$SCRIPT_DIR")
 
-python $SCRIPT_DIR/database_migration/database_migration.py -t delete_unbound_obj
-python $SCRIPT_DIR/database_migration/database_migration.py -t task_html_entry
-python $SCRIPT_DIR/database_migration/database_migration.py -t subtask_annotation
+python $BASEDIR/app/database_migration.py -t delete_unbound_obj
+python $BASEDIR/app/database_migration.py  -t task_html_entry
+python $BASEDIR/app/database_migration.py  -t subtask_annotation
