782d5e34ee6dcf2e5109d346b7b61bdc9f80175e
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2021-09-28 14:57:12 +0200

    changed shell scripts

--- scripts/start_mongo_db.sh
+++ scripts/start_mongo_db.sh
@@ -7,7 +7,7 @@ ENV_FILE_DIR="${BASEDIR}/app/secrets/local/mongodb-local.env"
 docker run -p 27017:27017 -v $BASEDIR/volumes:/data/db \
     -e MONGO_INITDB_ROOT_USERNAME=$MONGODB_USERNAME \
     -e MONGO_INITDB_ROOT_PASSWORD=$MONGODB_PASSWORD \
-    -e MONGO_INITDB_DATABASE=$MONGODB_INITDB_DATABASE \
+    -e MONGO_INITDB_DATABASE=$MONGO_INITDB_DATABASE \
     -e MONGODB_DATA_DIR=/data/db \
     -e MONDODB_LOG_DIR=/dev/null \
     mongo:4.4.4
