fe17bab09c0abd1f4e7edf58c5f4b2ce6137f9f9
Michael Gebauer | TU <michael.gebauer@tu-berlin.de>
2021-11-22 16:45:18 +0100

    revert

--- app/config.py
+++ app/config.py
@@ -9,7 +9,6 @@ class Config(object):
     # Secrets
     BASE_PATH = os.path.abspath(os.path.dirname(__file__))
     DEPLOYMENT = os.environ.get("DEPLOYMENT", False)
-    LOCAL_HOST = "http://127.0.0.1/"
 
     # Flask-User Setup
     USER_APP_NAME = "TILTer"  # Shown in and email templates and page footers
